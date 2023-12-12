1dc7ef86ad4e8c71e7bbe381f8ec13cb1b8169f2
kheshore <kheshore@gmail.com>
2023-12-12 19:35:57 +0530

    Resume Updation
    Signed-off-by: kheshore <kheshore@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# portfolio
+Hey this is Kheshore J R
+"Tech enthusiast with a knack for crafting immersive user experiences and a flair for IoT engineering. Seeking a role to inject innovation, drive impactful contributions, and amplify company success through innovative  solutions"
+
+It's my repo of my personal porfolio website ✨
--- script/script.js
+++ script/script.js
@@ -18,17 +18,6 @@ document.addEventListener('DOMContentLoaded', function () {
         activeLink = navItem;
     };
 
-    // const fragment = window.location.hash;
-
-    // if (fragment) {
-    //     const targetNavItem = document.querySelector(
-    //         `a[href="${fragment}"] li`
-    //     );
-    //     setActiveNavLink(targetNavItem);
-    // }
-
-    // Mobile Menu
-
     const toggleMenu = () => {
         const menuSelector = document.querySelector('div[aria-label="menu"]');
 
@@ -54,33 +43,4 @@ document.addEventListener('DOMContentLoaded', function () {
             menuSelector.classList.add('translate-y-52');
         }
     });
-
-    // On scroll check active section
-    // const sections = document.querySelectorAll('section');
-    // let activeSection = 'hero';
-
-    // const isSectionInView = (section) => {
-    //     const rect = section.getBoundingClientRect();
-    //     return rect.top >= 0 && rect.top <= 100;
-    // };
-
-    // const updateSection = () => {
-    //     sections.forEach((section) => {
-    //         if (isSectionInView(section)) {
-    //             activeSection = section.id;
-    //             console.log(activeSection);
-    //         }
-    //     });
-    // };
-
-    // const handleScroll = () => {
-    //     updateSection();
-    //     activeLink = document.querySelector('.nav-items.active-nav-link');
-    //     const targetNavItem = document.querySelector(
-    //         `a[href="#${activeSection}"] li`
-    //     );
-    //     setActiveNavLink(targetNavItem);
-    // };
-
-    // window.addEventListener('scroll', handleScroll);
 });
